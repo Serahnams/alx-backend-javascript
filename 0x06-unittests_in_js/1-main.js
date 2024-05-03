203748c8285e6096d6ec767bb3d3a7d8c3aafc43
Serahnams <serahnams@gmail.com>
2024-05-03 02:30:21 +0000

    All Tasks

---
+++ 0x06-unittests_in_js/1-main.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+
+const calculateNumber = require("./1-calcul.js");
+
+console.log(calculateNumber('SUM', 1.4, 4.5))
+console.log("")
+console.log(calculateNumber('SUBTRACT', 1.4, 4.5))
+console.log("")
+console.log(calculateNumber('DIVIDE', 1.4, 4.5))
+console.log("")
+console.log(calculateNumber('DIVIDE', 1.4, 0))
